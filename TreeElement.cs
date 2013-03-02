5d36726f7cdb30b2ef50b4b7edf96d883d36a743
mdrobny <michdrob@gmail.com>
2013-03-02 15:45:08 +0100

    Proj depend

---
+++ TreeElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+	class TreeElement : Rectangle
+	{
+		private int id;
+		private int branchId;
+
+		public TreeElement() {
+			
+		}
+		
+
+	}
+}
